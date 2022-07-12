7ac465fa569424e23bfd4b1f6af1beac772c0d53
Reza <mr.naeemabadi@gmail.com>
2022-07-12 18:45:04 +0200

    Adding some code

---
+++ src/RealTimeClock.cpp
@@ -0,0 +1,291 @@
+#include "RealTimeClock.h"
+
+// Public methods
+bool RealTimeClock::Initialize(device_t device_type, TwoWire &bus)
+{
+  wire = &bus;
+
+  return checkDevice(device_type);
+}
+
+bool RealTimeClock::Initialize(TwoWire &bus)
+{
+  wire = &bus;
+  if (checkDevice(device_t::DS13xx))
+    return true;
+
+  if (checkDevice(device_t::MCP7941x))
+    return true;
+
+  if (checkDevice(device_t::PCF85263))
+    return true;
+
+  return false;
+}
+
+void RealTimeClock::SetTime(tm *time)
+{
+  uint8_t clockHalt = 0;
+  uint8_t osconEtc = 0;
+  switch (device)
+  {
+  case device_t::PCF85263:
+    uint8_t settings[4] = {0x2E, 0x01, 0xA4, 0x00};
+    writeBlock(address, 4, settings);
+    break;
+
+  case device_t::DS13xx:
+    stopClock();
+    clockHalt = 0x80; // Clock halt to be kept enabled for now
+    break;
+
+  case device_t::MCP7941x:
+    stopClock();
+    osconEtc = read((uint8_t)0x03) & 0x38;
+    break;
+
+  default:
+    return;
+  }
+
+  uint8_t values[7];
+
+  values[0] = uint2bcd(time->tm_sec) | clockHalt;
+  values[1] = uint2bcd(time->tm_min);
+  values[2] = uint2bcd(time->tm_hour);
+  values[3] = uint2bcd(time->tm_mday);
+  values[4] = device == device_t::PCF85263 ? uint2bcd(time->tm_wday) : uint2bcd(time->tm_wday + 1);
+  values[5] = uint2bcd(time->tm_mon + 1);
+  values[6] = uint2bcd(time->tm_year % 100);
+
+  writeBlock(address, 7, values);
+
+  startClock();
+}
+void RealTimeClock::GetTime(time_t *time)
+{
+  tm time_tm = tm();
+  GetTime(&time_tm);
+  *time = mktime(&time_tm);
+}
+
+void RealTimeClock::GetTime(tm *time)
+{
+  uint8_t values[7] = {};
+
+  // Find which register to read from
+  uint8_t reg = getRegister(timeFunc_t::TIME);
+  read(reg, 7, values);
+
+  time->tm_sec = bcd2uint(values[0] & 0x7f);
+  time->tm_min = bcd2uint(values[1] & 0x7f);
+
+  if (values[2] & 0x40)
+  {
+    // Twelve hour mode
+    time->tm_hour = bcd2uint(values[2] & 0x1f);
+    if (values[2] & 0x20)
+      time->tm_hour += 12; // Seems notation for AM/PM is user-defined
+  }
+  else
+    time->tm_hour = bcd2uint(values[2] & 0x3f);
+
+  if (device == device_t::PCF85263)
+  {
+    // Day of month is before day of week!
+    time->tm_mday = bcd2uint(values[3] & 0x3f);
+    time->tm_wday = (values[4] & 0x07); // Clock uses [0..6]
+  }
+  else
+  {
+    time->tm_wday = (values[3] & 0x07) - 1; // Clock uses [1..7]
+    time->tm_mday = bcd2uint(values[4] & 0x3f);
+  }
+
+  time->tm_mon = bcd2uint(values[5] & 0x1f) - 1; // Clock uses [1..12]
+
+  time->tm_year = bcd2uint(values[6]) + 100; // Assume 21st century
+  time->tm_yday = -1;
+}
+
+bool RealTimeClock::GetClock(tm *time, timeFunc_t func)
+{
+
+  if (device != device_t::MCP7941x && func != timeFunc_t::TIME)
+    return false; // not supported
+
+  // Find which register to read from
+  uint8_t reg = getRegister(func);
+
+  if (func == timeFunc_t::TIME)
+    GetTime(time);
+
+  else if (device == MCP7941x &&
+           (func == TIME_POWER_FAILED || func == TIME_POWER_RESTORED))
+    readMCPTimeSaver(time, reg);
+
+  else if (device == MCP7941x &&
+           (func == ALARM0 || func == ALARM1))
+    return false; // TODO: Find out what should be done here
+
+  return true;
+}
+
+// Private methods
+void RealTimeClock::startClock(void) const
+{
+  switch (device)
+  {
+  case device_t::PCF85263:
+    write(address, 0x00);
+    return;
+
+  case device_t::MCP7941x:
+    uint8_t data = read(address, 0x00);
+    if ((data & 0x80) == 0)
+      write(address, data | 0x80); // Enable start bit
+    break;
+
+  case device_t::DS13xx:
+    uint8_t data = read(address, 0x00);
+    if (data & 0x80)
+      write(address, data & 0x7F); // Clear clock halt
+    break;
+
+  default:
+    break;
+  }
+}
+
+void RealTimeClock::stopClock(void) const
+{
+  switch (device)
+  {
+  case device_t::PCF85263:
+    write(0x2E, 0x01);
+    break;
+
+  case device_t::DS13xx:
+    uint8_t data = read(address, 0x00);
+    write(address, data | 0x80);
+    break;
+
+  case device_t::MCP7941x:
+    uint8_t data = read(address, 0x00);
+    write(address, data & 0x7F);
+    break;
+
+  default:
+    break;
+  }
+}
+
+uint8_t RealTimeClock::getRegister(timeFunc_t func) const
+{
+  const uint8_t regTable[3][5] = {
+      {0, 0xff, 0xff, 0xff, 0xff}, // DS1307
+      {0, 0x0a, 0x11, 0x18, 0x1C}, // MCP7941x
+      {1, 0x08, 0x0d, 0xff, 0xff}, // PCF85263
+
+  };
+  return regTable[device][func];
+}
+
+void RealTimeClock::readMCPTimeSaver(struct tm *time, uint8_t reg, uint8_t sz) const
+{
+  if (sz != 4)
+    return;
+
+  uint8_t values[4] = {};
+  read(reg, sz, values);
+
+  time->tm_sec = 0;
+  time->tm_min = bcd2uint(values[0] & 0x7f);
+  time->tm_hour = bcd2uint(values[1] & 0x3f);
+  time->tm_wday = 0;
+  time->tm_mday = bcd2uint(values[2] & 0x3f);
+  uint8_t wdayMonth = values[3];
+  time->tm_mon = bcd2uint(wdayMonth & 0x1f) - 1; // Clock uses [1..12]
+  time->tm_wday = (wdayMonth >> 5) - 1;          // Clock uses [1..7]
+  time->tm_year = (YEAR_EPOCH - 1900);           // not stored
+  time->tm_yday = -1;
+}
+
+bool RealTimeClock::checkDevice(device_t device_type)
+{
+  if (device_type == device_t::NONE)
+    return false;
+
+  device = device_type;
+  address = deviceAddresses[(uint8_t)device_type];
+
+  wire->beginTransmission(address);
+  wire->write(uint8_t(0));
+  wire->endTransmission();
+  wire->requestFrom(address, (uint8_t)1);
+  return wire->available();
+}
+
+void RealTimeClock::read(uint8_t _register, uint8_t length, uint8_t *values)
+{
+  wire->beginTransmission(address);
+  wire->write(_register);
+  wire->endTransmission();
+  wire->requestFrom(address, length);
+  for (uint8_t i = 0; i < length; i++)
+    values[i] = wire->read();
+}
+
+uint8_t RealTimeClock::read(uint8_t _register)
+{
+  wire->beginTransmission(address);
+  wire->write(_register);
+  wire->endTransmission();
+  wire->requestFrom(address, (uint8_t)1);
+  return wire->read();
+}
+
+void RealTimeClock::write(uint8_t _register, uint8_t value)
+{
+  wire->beginTransmission(address);
+  wire->write((uint8_t)_register);
+  wire->write((uint8_t)value);
+  wire->endTransmission();
+}
+
+void RealTimeClock::writeBlock(uint8_t _register, uint8_t length, uint8_t *value)
+{
+  wire->beginTransmission(address);
+  wire->write((uint8_t)_register);
+  for (size_t i = 0; i < length; i++)
+    wire->write((uint8_t)value[i]);
+
+  wire->endTransmission();
+}
+
+void RealTimeClock::write(uint8_t _register, uint8_t &value, uint8_t &mask)
+{
+  uint8_t val = (read(_register) & mask) | value;
+  wire->beginTransmission(address);
+  wire->write((uint8_t)_register);
+  wire->write((uint8_t)val);
+  wire->endTransmission();
+}
+
+uint8_t RealTimeClock::uint2bcd(uint8_t uint_val)
+{
+  return uint_val + 6 * (uint_val / 10);
+}
+
+uint8_t RealTimeClock::bcd2uint(int8_t bcd_val)
+{
+  return bcd_val - 6 * (bcd_val >> 4);
+}
+
+uint8_t RealTimeClock::bcd2uint24Hour(uint8_t bcdHour)
+{
+  if (bcdHour & 0x40)
+    return (bcdHour & 0x20) ? bcd2uint(bcdHour & 0x1f) + 12 : bcd2uint(bcdHour & 0x1f);
+
+  return bcd2uint(bcdHour);
+}
---
+++ src/RealTimeClock.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <Arduino.h>
+#include <Wire.h>
+
+#ifndef YEAR_EPOCH
+#define YEAR_EPOCH 1970
+#endif
+
+class RealTimeClock
+{
+public:
+	typedef enum
+	{
+		DS13xx = 0, // DS1307, DS1337, DS1338
+		MCP7941x = 1,
+		PCF85263 = 2,
+		NONE = 127,
+	} device_t;
+
+	typedef enum
+	{
+		TIME = 0,
+		ALARM0 = 1,
+		ALARM1 = 2,
+		TIME_POWER_FAILED = 3,
+		TIME_POWER_RESTORED = 4,
+	} timeFunc_t;
+
+	typedef enum
+	{
+		freq1Hz = 0,
+		freq4096Hz = 1,
+		freq8192Hz = 2,
+		freq32768Hz = 3,
+		freqCalibration = 4, // device-specific calibration for MCP7941x
+
+		// Additional values for PCF85263
+		freq1024Hz = 5,
+		freq2048Hz = 6,
+		freq16384Hz = 7,
+		freqOutputLow = 8,
+	} freq_t;
+
+	bool Initialize(TwoWire &bus = Wire);
+	bool Initialize(device_t device, TwoWire &bus = Wire);
+
+	void GetTime(time_t *time);
+	void GetTime(tm *time);
+	void SetTime(tm *time);
+	bool GetClock(tm *time, timeFunc_t func); // TODO: Make it better
+
+	bool IsLeapYear();
+
+private:
+	const uint8_t deviceAddresses[3] = {0x68, 0x6F, 0x51};
+
+	uint8_t address;
+	device_t device;
+	TwoWire *wire;
+
+	bool checkDevice(device_t dev);
+	uint8_t getRegister(timeFunc_t func) const;
+	void readMCPTimeSaver(struct tm *tm, uint8_t reg) const;
+	void stopClock(void) const;
+	void startClock(void) const;
+
+	void read(uint8_t _register, uint8_t length, uint8_t *values);
+	uint8_t read(uint8_t _register);
+	void write(uint8_t _register, uint8_t value);
+	void write(uint8_t _register, uint8_t &value, uint8_t &mask);
+	void writeBlock(uint8_t _register, uint8_t length, uint8_t *value);
+
+	uint8_t uint2bcd(uint8_t int_val);
+	uint8_t bcd2uint(int8_t bcd_val);
+	uint8_t bcd2uint24Hour(uint8_t bcdHour);
+};
